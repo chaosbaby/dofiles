522a0778383f37a6a5f94aaa056dec78ff3d3af5
root <root@localhost.localdomain>
2019-04-13 09:20:13 +0800

    fix python install pure mini

--- python3/setup_3.7.3.sh
+++ python3/setup_3.7.3.sh
@@ -1,8 +1,15 @@
 yum install zlib zlib-devel -y
 yum install libffi-devel -y
+yum install unzip wget -y
+yum install ctags cmake gcc-c++ python-devel -y
 cd ~/tool
-# wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz                                                                                
-# tar -xvJf  Python-3.7.3.tar.xz
+if [ -d "/root/tool/python-3.7.3/" ];then
+echo "文件夹存在"
+else
+echo "文件夹不存在"
+wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz                                                                                
+tar -xvJf  Python-3.7.3.tar.xz
+fi
 mkdir /usr/local/python3 #创建编译安装目录
 cd Python-3.7.3
 ./configure --prefix=/usr/local/python3
