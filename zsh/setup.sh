3927247e1ed754ae201fd23dfbd1cecaeda55222
root <root@localhost.localdomain>
2019-04-13 11:56:08 +0800

    fix zsh install sh

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
-
+yum install zsh -y
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 if [[ ! -d ~/.oh-my-zsh ]]; then
     echo "Install oh-my-zsh ..."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
